395170ba0feb514e1307de86ca22e6c1d7a527eb
Phai <norapat_c@cmu.ac.th>
2023-08-10 15:28:12 +0700

    comitted

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -8,29 +8,32 @@ export const Comment = ({
   replies,
 }) => {
   return (
-    <div className="d-flex gap-2 my-2">
-      <img
-        src={userImagePath}
-        width="48"
-        height="48"
-        className="rounded-circle"
-        style={{ objectFit: "cover" }}
-      />
-      <div
-        className="rounded rounded-3 p-2"
-        style={{ backgroundColor: "#E5E7EB" }}
-      >
-        <span className="fw-semibold">{username}</span>
-        <br />
-        <span>{commentText}</span>
-        <div className="d-flex align-items-center gap-1">
-          <img src="/like.svg" width={20}></img>
-          <span className="text-muted">{likeNum} คน</span>
+    <div>
+      <div className="d-flex gap-2 my-2">
+        <img
+          src={userImagePath}
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <div
+          className="rounded rounded-3 p-2"
+          style={{ backgroundColor: "#E5E7EB" }}
+        >
+          <span className="fw-semibold">{username}</span>
+          <br />
+          <span>{commentText}</span>
+          <div className="d-flex align-items-center gap-1">
+            <img src="/like.svg" width={20}></img>
+            <span className="text-muted">{likeNum} คน</span>
+          </div>
         </div>
       </div>
       {/* map-loop render Reply component here */}
       {replies.map((rep) => (
         <Reply
+          key={rep.userImagePath}
           userImagePath={rep.userImagePath}
           username={rep.username}
           replyText={rep.replyText}
